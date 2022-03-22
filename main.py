0c29f885b1c22ab260968b1944f903ba732b0a6c
juvilla <92930948+juvilla@users.noreply.github.com>
2022-03-22 09:39:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+class Asiento:
+    def _init_(self,color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    def cambiarColor(self,color):
+        if color=="rojo":
+            self.color=color
+        elif color=="verde":
+            self.color=color
+        elif color=="amarillo":
+            self.color=color
+        elif color=="negro":
+            self.color=color
+        elif color=="blanco":
+            self.color=color
+class Auto:
+    cantidadCreados=0
+    def _init_(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+    def cantidadAsientos(self):
+        ct=0
+        for i in self.asientos:
+            if i==None:
+                continue
+            else:
+                ct+=1
+        return ct
+    def verificarIntegridad(self):
+        if self.registro!=self.motor.registro:
+            return "Las piezas no son originales"
+        for i in self.asientos:
+            if i==self.registro:
+                continue
+            else:
+                return "Las piezas no son originales"
+        return "Auto original"      
+class Motor:
+    def _init_(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+    def cambiarRegistro(self,registro):
+        self.registro=registro
+    def asignarTipo(self,tipo):
+        if tipo=="electrico" or tipo=="gasolina":
+            self.tipo=tipo
+    
